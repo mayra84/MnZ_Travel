6830f92fca04dcdbf495f245fbadf806130b4436
zaria <mikeburrows8114@gmail.com>
2021-12-18 13:52:26 -0500

    took search out of header since it will still be on the main page, changed API key to my own

--- locations.js
+++ locations.js
@@ -1,4 +1,4 @@
-// API KEY:AIzaSyCTyX4izBaLSUMvtZMxEjOCPo_IpDJITRs
+// API KEY:8f32460e07msh82e48d9017e28a2p1a1156jsncedaa79245ad
 
 
 // ask lachlan about it tmrw
@@ -29,7 +29,7 @@ searchForm.addEventListener('submit', function (event) {
         "method": "GET",
         "headers": {
             "x-rapidapi-host": "imdb8.p.rapidapi.com",
-            "x-rapidapi-key": "750787b786msh3494b73242ba7b4p1baff1jsnca241a92c7a4"
+            "x-rapidapi-key": "8f32460e07msh82e48d9017e28a2p1a1156jsncedaa79245ad"
         }
     })
         .then(function (response) {
